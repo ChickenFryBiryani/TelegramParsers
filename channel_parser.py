b1d385fb787ca4c5265f9e922ed9e15cf4424695
ChickenFryBiryani <chickenfrybiryani@gmail.com>
2020-10-28 11:37:04 -0400

    Telegram parser for public channels

---
+++ channel_parser.py
@@ -0,0 +1,68 @@
+# ChickenFryBiryani
+
+import json
+import datetime
+import database_connector
+
+chat_folder_path = "/home/rob/Downloads/Telegram Desktop/ChatExport_2020-10-27/"
+telegram_channel_id = ""
+
+
+def decodeStr(string):
+    return string.encode('unicode-escape').decode("utf-8")
+
+
+def getDateString(date_str):
+    return datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S').strftime('%Y%m%d%H%M%S')
+
+
+def getMediaPath(message):
+    if 'photo' in message:
+        return str(telegram_channel_id) + '/' + chat_folder_path.split('/')[-2] + '/' + message['photo']
+    if 'file' in message:
+        return str(telegram_channel_id) + '/' + chat_folder_path.split('/')[-2] + '/' + message['file']
+    return ''
+
+
+def getText(message):
+    if 'text' not in message or message['text'] == '':
+        return ''
+    if type(message['text']) == str:
+        return decodeStr(message['text'])
+    return ' '.join(list(map(lambda x: decodeStr(x).strip() if type(x) == str else decodeStr(x['text']).strip(),
+                             message['text'])))
+
+
+def main():
+    global telegram_channel_id
+    with open(chat_folder_path + 'result.json') as chat_file:
+        chat_content = json.load(chat_file)
+    if chat_content['type'] != 'public_channel':
+        print('Not a channel.')
+        return
+    telegram_channel_id = chat_content['id']
+    telegram_channel_name = decodeStr(chat_content['name'])
+    telegram_db = database_connector.mySQLTelegramDB()
+    search_response = telegram_db.get_id_from_telegram_id(False, telegram_channel_id)
+    if not search_response:
+        return
+    if search_response > 0:
+        channel_id = search_response
+    else:
+        add_response = telegram_db.add_group_channel(False, telegram_channel_id, telegram_channel_name)
+        if not add_response:
+            return
+        channel_id = add_response
+    from_msg_id = telegram_db.get_last_added_msg_id(channel_id)
+    if not from_msg_id:
+        return
+    pending_msgs = list(filter(lambda x: x['id'] >= from_msg_id and x['type'] == 'message', chat_content['messages']))
+    insert_msgs = list(map(lambda x: (channel_id, str(x['id']), getDateString(x['date']),
+                                      getText(x), getMediaPath(x)), pending_msgs))
+    rows_added = telegram_db.add_channel_chat_posts(insert_msgs)
+    print('Chat posts added: ', rows_added)
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ database_connector.py
@@ -0,0 +1,95 @@
+# ChickenFryBiryani
+import time
+import mysql
+import mysql.connector
+
+
+class mySQLTelegramDB:
+    def __init__(self):
+        self.m_sServerHost = "jaguar.cs.gsu.edu"
+        self.m_sServerUser = "covid19"
+        self.m_sServerPasswd = "uvNjdEbsn3t5uyiQkXgw"
+        self.m_sServerDatabaseUser = "telegram_admin"
+        self.m_sServerDatabasePasswd = "wowiNrNaTjObof5n6YGm"
+        self.m_sServerDatabase = "telegram"
+        self.m_sServerPort = "3306"
+        self.telegram_conn = None
+        self.telegram_cursor = None
+
+    def get_db_connection(self):
+        retries = 5
+        while retries != 0:
+            try:
+                self.telegram_conn = mysql.connector.connect(host=self.m_sServerHost, user=self.m_sServerDatabaseUser,
+                                                        passwd=self.m_sServerDatabasePasswd,
+                                                        database=self.m_sServerDatabase, port=self.m_sServerPort)
+                self.telegram_cursor = self.telegram_conn.cursor()
+                return True
+            except:
+                time.sleep(5)
+                retries -= 1
+        print('Jaguar Connectivity issues.')
+        return False
+
+    def close_db_connection(self):
+        # if self.telegram_cursor:
+        #     self.telegram_cursor.close()
+        if self.telegram_conn:
+            self.telegram_conn.close()
+
+    def get_id_from_telegram_id(self, isGroup, telegram_id):
+        if not self.get_db_connection():
+            return False
+        g_type = 'group' if isGroup else 'channel'
+        query = "SELECT group_channel_id FROM groups_channels WHERE group_or_channel = '{}' AND " \
+                "telegram_id_of_gp_or_ch = '{}';".format(g_type, telegram_id)
+        self.telegram_cursor.execute(query)
+        count = self.telegram_cursor.fetchone()
+        if not count:
+            self.close_db_connection()
+            return -1
+        result_id = count[0]
+        self.close_db_connection()
+        return result_id
+
+    def add_group_channel(self, isGroup, telegram_id, telegram_name):
+        # False: Not added
+        # >0 : id
+        if not self.get_db_connection():
+            return False
+        g_type = 'group' if isGroup else 'channel'
+        qry = "INSERT INTO groups_channels (group_or_channel_name, group_or_channel, telegram_id_of_gp_or_ch) VALUES " \
+              "('{}', '{}', '{}');".format(telegram_name, g_type, telegram_id)
+        self.telegram_cursor.execute(qry)
+        if self.telegram_cursor.rowcount == 1:
+            return_id = int(self.telegram_cursor.lastrowid)
+            self.telegram_conn.commit()
+            self.close_db_connection()
+            return return_id
+        self.telegram_conn.rollback()
+        self.close_db_connection()
+        return False
+
+    def get_last_added_msg_id(self, channel_or_group_id):
+        if not self.get_db_connection():
+            return False
+        query = "SELECT telegram_msg_id FROM chat_posts WHERE group_channel_id = '{}' ORDER BY " \
+                "post_id DESC LIMIT 1".format(channel_or_group_id)
+        self.telegram_cursor.execute(query)
+        if self.telegram_cursor.rowcount == 1:
+            return_val = int(self.telegram_cursor.fetchone())
+        else:
+            return_val = -1
+        self.close_db_connection()
+        return return_val
+
+    def add_channel_chat_posts(self, message_list):
+        if not self.get_db_connection():
+            return False
+        query = """INSERT INTO chat_posts (group_channel_id, telegram_msg_id, post_date, post_text, media_path) VALUES 
+        (%s, %s, %s, %s, %s)"""
+        self.telegram_cursor.executemany(query, message_list)
+        rows_inserted = self.telegram_cursor.rowcount
+        self.telegram_conn.commit()
+        self.close_db_connection()
+        return rows_inserted
